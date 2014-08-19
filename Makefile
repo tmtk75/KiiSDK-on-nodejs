130d2b07a117d9b456d2ba0b2965e9d9b905cc87
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-08-19 12:03:40 +0900

    initial import

---
+++ Makefile
@@ -0,0 +1,6 @@
+app_id=47c5f35d 
+app_key=d1ceb3f0d4636bc9cc8aef47dc62d5f5 
+
+run:
+	APP_ID=$(app_id) APP_KEY=$(app_key) \
+	node main.js
---
+++ main.js
@@ -0,0 +1,21 @@
+require("node-jquery-xhr");
+var KiiSDK = require("./KiiSDK.js");
+var root = KiiSDK.create();
+["Kii", "KiiUser", "KiiSite"].map(function(e) { eval(e + " = root." + e) });
+
+var app_id = process.env.APP_ID,
+    app_key = process.env.APP_KEY;
+
+Kii.initializeWithSite(app_id, app_key, KiiSite.CN);
+
+var username = "user123456";
+var password = "123abc";
+var user = KiiUser.userWithUsername(username, password);
+user.register({
+  success: function(u) {
+    console.log("uuid: ", u.getUUID());
+  },
+  failure: function(u, errstr) {
+    console.log("error: " + errstr);
+  }
+});
